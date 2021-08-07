82326c67c5cd5aeb35df3fbd7d59bf652503525c
Char Isabelo <charisseisabelo@gmail.com>
2021-08-06 16:41:26 -1000

    completed bio comments functionality

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,4 +1,4 @@
-const commentsArray = [
+let commentsArray = [
   {
     name: "Connor Walton",
     timeStamp: "02/17/2021",
@@ -18,7 +18,7 @@ const commentsArray = [
   },
 ];
 
-const commentsList = document.querySelector(".comments__list");
+let commentsList = document.querySelector(".comments__list");
 const submit = document.querySelector(".comments__form");
 
 const displayComment = (commentObj) => {
@@ -26,39 +26,33 @@ const displayComment = (commentObj) => {
   const commentsListCard = document.createElement("div");
   commentsListCard.classList.add("comments__list-card");
   commentsList.appendChild(commentsListCard);
-  console.log(commentsListCard);
 
   // create comments__list-img
   const commentsListImg = document.createElement("div");
   commentsListImg.classList.add("comments__list-img");
   commentsListCard.appendChild(commentsListImg);
-  console.log(commentsListImg);
 
   // create comments__list-details
   const commentsListDetails = document.createElement("div");
   commentsListDetails.classList.add("comments__list-details");
   commentsListCard.appendChild(commentsListDetails);
-  console.log(commentsListDetails);
 
   // create comments__list-user
   const commentsListUser = document.createElement("div");
   commentsListUser.classList.add("comments__list-user");
   commentsListDetails.appendChild(commentsListUser);
-  console.log(commentsListUser);
 
   // create comments__user-name
   const commentsUserName = document.createElement("p");
   commentsUserName.classList.add("comments__user-name");
   commentsUserName.innerText = commentObj.name;
   commentsListUser.appendChild(commentsUserName);
-  console.log(commentsUserName);
 
   // create comments__user-date
   const commentsUserDate = document.createElement("p");
   commentsUserDate.classList.add("comments__user-date");
   commentsUserDate.innerText = commentObj.timeStamp;
   commentsListUser.appendChild(commentsUserDate);
-  console.log(commentsUserDate);
 
   // create comments__list-text
   const commentsListText = document.createElement("p");
@@ -73,13 +67,36 @@ commentsArray.forEach((comment) => {
   return displayComment(comment);
 });
 
-submit.addEventListener("submit", function (event) {
+let newCommentsArray = [];
+
+const onSubmitValue = (event) => {
   event.preventDefault();
-
   console.log(event);
-});
+  const nameField = document.querySelector(".comments__name-input").value;
+  const commentField = document.querySelector(".comments__form-comment").value;
 
-let one = () => {
-  let two = document.querySelector(".comments__name-input").value;
-  console.log(two);
+  let newObj = {
+    name: nameField,
+    timeStamp: "07/05/2021",
+    comment: commentField,
+  };
+
+  commentsList.innerHTML = ``;
+  //   console.log(commentsList);
+
+  newCommentsArray.unshift(newObj);
+  //   console.log(newCommentsArray);
+
+  let concatedArray = newCommentsArray.concat(commentsArray);
+
+  console.log("concated", concatedArray);
+
+  concatedArray.forEach((item) => {
+    return displayComment(item);
+  });
+
+  const form = document.querySelector("form");
+  form.reset();
 };
+
+submit.addEventListener("submit", onSubmitValue);
